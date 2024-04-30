7c6b41daf5db9b3ea37f9a8e202ade69e9f8049a
DeveloperAromal <153208245+DeveloperAromal@users.noreply.github.com>
2024-04-30 20:12:06 +0530

    updated hero

--- app/components/includes/Navbar.tsx
+++ app/components/includes/Navbar.tsx
@@ -36,7 +36,7 @@ export default function Navbar() {
   return (
     <section className="pb-20">
       <div>
-        <nav className="backdrop-blur-md h-20 w-full fixed top-0 left-0 right-0 lg:flex lg:justify-between lg:items-center lg:px-20">
+        <nav className="backdrop-blur-sm h-20 w-full fixed top-0 left-0 right-0 lg:flex lg:justify-between lg:items-center lg:px-20">
           <div className="flex justify-between items-center">
             <Link href="/home">
               <div className="cursor-pointer ">
