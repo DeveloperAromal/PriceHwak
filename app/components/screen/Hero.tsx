084a34ef7acd83de862686e26a7582152634283a
Nythikniya <165932438+Nythikniya@users.noreply.github.com>
2024-04-30 14:33:43 +0530

    Update Hero.tsx

--- app/components/screen/Hero.tsx
+++ app/components/screen/Hero.tsx
@@ -1,7 +1,18 @@
 export default function Hero(){
     return(
         <section>
-            Hero section
+             <section className="top">
+                <div className="top text-center pt-6 pb-5">
+                    <h1 className="mt-10 mb-2">Your Money's Important</h1>
+                    <h3>so don't waste time</h3>
+                    <p>with pricewice,you can track e-commerce product price <br/> 
+                    and you can plan perfectly on when to buy these</p>
+                    <button>Try a demo</button>
+                </div>
+                <div className="bottom">
+
+                </div>
+            </section>
         </section>
     );
 }
