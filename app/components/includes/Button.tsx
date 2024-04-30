456fd5d03dbce0f55b5b6869c010b64cbb25aad3
Mr.Sabith.A5 <businessmansabith@gmail.com>
2024-04-30 06:13:24 +0000

    changed one more time

--- app/components/includes/Button.tsx
+++ app/components/includes/Button.tsx
@@ -9,11 +9,11 @@ type ButtonProps = {
 const Button = ({title,href}:ButtonProps) => {
   return (
 
-    <button className='py-3 px-5 rounded-xl text-black bg-white font-bold'>
-        <Link href={href}>
+    <Link href={href} className='py-3 px-5 rounded-xl text-black bg-white font-bold'>
+        <button>
                 {title}
-        </Link>
-    </button>
+        </button>
+    </Link>
   )
 }
 
