8a866bbc11a02c5395b56262d5e5777bba89390f
DeveloperAromal <153208245+DeveloperAromal@users.noreply.github.com>
2024-04-30 19:50:53 +0530

    updated hero

--- app/components/screen/Hero.tsx
+++ app/components/screen/Hero.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Navbar from "../includes/Navbar";
 import Button from "../includes/Button";
+import "@fontsource/dancing-script";
+
 
 export default function Hero(){
     return(
@@ -10,19 +12,19 @@ export default function Hero(){
             </div>
              <section className="top">
                 <div className="top text-center pt-6 pb-5">
-                    <h1 className="mt-5  text-7xl underline">Your Money's Important</h1>
-                    <h3 className="mt-4 text-5xl underline italic">so don't waste time</h3>
-                    <p className="mt-4 text-2xl mb-3">with pricewice,you can track e-commerce product price <br/> 
+                    <h1 className="mt-5  text-7xl">Your Money's Important</h1>
+                    <h3 className="mt-4 text-5xl italic">so don't waste time</h3>
+                    <p className="mt-4 text-2xl mb-3" style={{ fontFamily: "Dancing Script" }}>with pricewice,you can track e-commerce product price <br/> 
                     and you can plan perfectly on when to buy these</p>
-                    <Button href="#" title="Try a demo" />
+                    <Button href="/auth/v1/signup" title="Try a demo" />
                 </div>
-                <div className="bottom">
+                <div className="flex items-center justify-center">
                     <Image
-                     src="/assets/images/github.png"
+                      src="/assets/images/github.png"
                       alt="github" 
                       width={1300}
                       height={25}
-                      className="ml-5"
+                      className=""
                       />
                 </div>
             </section>
