63e7da39cd12fa5c0b3c30aabd1ff22d00e7112e
DeveloperAromal <153208245+DeveloperAromal@users.noreply.github.com>
2024-04-30 20:14:28 +0530

    updated hero

--- app/components/screen/Hero.tsx
+++ app/components/screen/Hero.tsx
@@ -12,8 +12,8 @@ export default function Hero(){
             </div>
              <section className="top">
                 <div className="top text-center pt-6 pb-5">
-                    <h1 className="mt-5 text-4xl lg:text-7xl md:text-5xl sm:text-4xl">Your Money's Important</h1>
-                    <h3 className="mt-4 text-3xl lg:text-6xl md:text-4xl sm:text-3xl" style={{ fontFamily: "Dancing Script" }}>so don't waste time</h3>
+                    <h1 className="mt-5 text-3xl lg:text-7xl md:text-5xl sm:text-4xl">Your Money's Important</h1>
+                    <h3 className="mt-4 text-2xl lg:text-6xl md:text-4xl sm:text-3xl" style={{ fontFamily: "Dancing Script" }}>so don't waste time</h3>
                     <p className="mt-4 text-center text-xl lg:text-2xl md:text-2xl sm:text-2xl mb-3">with pricewice,you can track e-commerce product price <br/> 
                     and you can plan perfectly on when to buy these</p>
                     <Button href="/auth/v1/signup" title="Try a demo" />
