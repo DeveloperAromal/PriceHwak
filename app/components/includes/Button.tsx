913e57165d3b990f21f224c656fb31eef7850877
Mr.Sabith.A5 <businessmansabith@gmail.com>
2024-04-30 06:10:47 +0000

    changed button hierarchy

--- app/components/includes/Button.tsx
+++ app/components/includes/Button.tsx
@@ -8,9 +8,12 @@ type ButtonProps = {
 
 const Button = ({title,href}:ButtonProps) => {
   return (
-    <Link href={href} className='py-3 px-5 rounded-xl text-black bg-white font-bold'>
-        {title}
-    </Link>
+
+    <button className='py-3 px-5 rounded-xl text-black bg-white font-bold'>
+        <Link href={href}>
+                {title}
+        </Link>
+    </button>
   )
 }
 
