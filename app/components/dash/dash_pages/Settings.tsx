8a53f74d7a568cb77a9d88803f87178a95220a9b
DeveloperAromal <153208245+DeveloperAromal@users.noreply.github.com>
2024-05-06 11:06:25 +0530

    updated settings

--- app/components/dash/dash_pages/Settings.tsx
+++ app/components/dash/dash_pages/Settings.tsx
@@ -3,7 +3,7 @@ export default function Settings(){
         <section>
             <div>
                <h1 className="text-center py-20 text-3xl">
-               This is the settings page please edit <span className="text-teal-500">settings.tsx</span> in the <span className="text-red-400">components/dash/dash_pages/Settings.tsx</span>
+               This is the Settings page please edit <span className="text-teal-500">Settings.tsx</span> in the <span className="text-red-400">components/dash/dash_pages/Settings.tsx</span>
                </h1>
             </div>
         </section>
