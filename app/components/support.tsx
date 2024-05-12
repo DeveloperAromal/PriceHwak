7ef4aa34b0c34018f321ef3322552879f9bdb297
Azaan Anilbabu <165854850+AZUG47EX@users.noreply.github.com>
2024-05-12 13:25:59 +0400

    sipp

---
+++ app/components/support.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Button from "./includes/Button";
+import {
+  main,
+  Image2,
+  Groups,
+  Image10,
+  ContactUs,
+  Button3,
+  Background4,
+  Image9,
+  phoneno,
+  Button2,
+  Image8,
+  Ceopricehawkco,
+  Button as CustomButton,
+  Location,
+  Image7,
+  Groups2,
+  ConnectWithUs,
+  Image6,
+  Image5,
+  Image4,
+  Image3,
+} from "app/support/styles.tsx";
+
+const main: React.FC = (props) => {
+  return (
+    <section className="flex justify-center items-center min-h-screen w-full pt-20">
+      <div>
+        <div className="flex flex-wrap justify-center gap-4">
+          <div className="w-40 h-40 absolute top-0 md:right-2/4 rounded-full bg-pink-800 blur">.</div>
+          <main className="w-full flex min-h-435px items-center bg-cover justify-center bg-rgba-0-0-0-0 pt-4 pb-7px px-4" backgroundImage="https://assets.rapidream.com/project/735303ce-4bb6-4afd-b2d7-34be2c33d6af/assets/72dd38ab-5368-41db-8959-de4a883f85e1.png">
+            <Image2 className="w-98-06 bg-cover min-h-396px self-start flex items-center mt-4 pt-1 pb-11px px-4" backgroundImage="https://assets.rapidream.com/project/735303ce-4bb6-4afd-b2d7-34be2c33d6af/assets/fdf6de74-67a9-4842-9c29-a86468b80790.png">
+              <Groups className="w-47-52 bg-cover bg-rgba-0-0-0-0 min-h-381px relative ml-52-48" backgroundImage="https://assets.rapidream.com/project/735303ce-4bb6-4afd-b2d7-34be2c33d6af/assets/8ce97d76-7ed8-4502-a90c-70a50e1e5148.png">
+                <Image10 className="w-11 bg-cover min-h-11px absolute left-90-19 top-18px" backgroundImage="https://assets.rapidream.com/project/735303ce-4bb6-4afd-b2d7-34be2c33d6af/assets/9f196b84-f6bd-4d56-8ee9-8a723764fe4f.png" />
+                <ContactUs className="text-rgba-150-156-154-1 text-10px font-normal whitespace-pre-wrap tracking-normal no-underline absolute left-66-6 top-18px">contact us</ContactUs>
+                <Button3 className="w-88-68 min-h-35px absolute flex justify-center left-5-85 top-138-5px">
+                  <Background4 className="w-full border min-h-35px flex flex-row pl-15px pr-4 py-2-5 rounded-3-25px-3-25px-3-25px-3-25px border-rgba-70-113-89-1 border-solid" backgroundImage="https://assets.rapidream.com/project/735303ce-4bb6-4afd-b2d7-34be2c33d6af/assets/318c7806-9fd6-4bbe-bb98-169615f4496f.png">
+                    <Image9 backgroundImage="https://assets.rapidream.com/project/735303ce-4bb6-4afd-b2d7-34be2c33d6af/assets/318c7806-9fd6-4bbe-bb98-169615f4496f.png" />
+                    <phoneno>+91879783738</phoneno>
+                  </Background4>
+                </Button3>
+                <Button2 className="w-89-06 bg-cover bg-rgba-0-0-0-0 min-h-36px absolute flex flex-row pl-15px pr-4 pt-2-5 pb-3 rounded-3-25px left-5-66 top-180px align-items-start" backgroundImage="https://assets.rapidream.com/project/735303ce-4bb6-4afd-b2d7-34be2c33d6af/assets/4db20faf-0577-4486-993f-eb51c6e90d32.png">
+                  <Image8 className="w-15 bg-cover min-h-11px mt-3px" backgroundImage="https://assets.rapidream.com/project/735303ce-4bb6-4afd-b2d7-34be2c33d6af/assets/6a705e2c-a070-4ea9-849a-1ef189618a54.png" />
+                  <Ceopricehawkco>ceo@pricehawk.com</Ceopricehawkco>
+                </Button2>
+                <CustomButton className="w-89-06 bg-cover bg-rgba-0-0-0-0 min-h-67px absolute rounded-3px left-5-66 top-223px" backgroundImage="https://assets.rapidream.com/project/735303ce-4bb6-4afd-b2d7-34be2c33d6af/assets/d113ea61-8d33-4147-8566-15a4155298ba.png">
+                  <Location>enthelum location itt,</Location>
+                  <Image7 className="w-11 bg-cover min-h-15px absolute left-7-2 top-2-5" backgroundImage="https://assets.rapidream.com/project/735303ce-4bb6-4afd-b2d7-34be2c33d6af/assets/106e0eaa-e790-468f-884e-06a106f5a9a8.png" />
+                </CustomButton>
+                <Groups2 className="w-61-51 min-h-46-5px absolute left-5-66 top-316px">
+                  <ConnectWithUs>connect with us on</ConnectWithUs>
+                  <Image6 className="w-65 bg-cover min-h-1px absolute left-0 top-7" backgroundImage="https://assets.rapidream.com/project/735303ce-4bb6-4afd-b2d7-34be2c33d6af/assets/cabb2c95-71c6-4f02-ad3d-e6e8dc84115f.png" />
+                  <Image5 backgroundImage="https://assets.rapidream.com/project/735303ce-4bb6-4afd-b2d7-34be2c33d6af/assets/e9205d64-7f7f-4aa4-8fc0-921a490677d1.png" />
+                  <Image4 backgroundImage="https://assets.rapidream.com/project/735303ce-4bb6-4afd-b2d7-34be2c33d6af/assets/1b7034bb-ce6f-4f93-97f1-0da0368376d1.png" />
+                  <Image3 backgroundImage="https://assets.rapidream.com/project/735303ce-4bb6-4afd-b2d7-34be2c33d6af/assets/94cbcdba-c781-4bfc-afff-583b6f980392.png" />
+                </Groups2>
+              </Groups>
+            </Image2>
+          </main>
+      
+        </div>
+        
+      </div>
+    </section>
+  );
+};
+
+export default main05212AMJpeg;
---
+++ app/support/page.tsx
@@ -0,0 +1,9 @@
+import Pricing from "../components/support"
+
+export default function main(){
+  return(
+    <section>
+        <Support/>
+    </section>
+  );
+}
